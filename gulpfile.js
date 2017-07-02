bb43f71d4fa577bdcd190e39a4601780267e0546
Ianis G. Vasilev <ianis@ivasilev.net>
2017-07-02 22:18:58 +0300

    Fix breaking gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
 /* eslint-env node */
 
-const gulp = require('gulp');
-const livereload = require('gulp-livereload');
+const gulp = require('gulp')
+const livereload = require('gulp-livereload')
 
-require('./build/client');
-require('./build/server');
-const armor = require('./build/armor');
+require('./build/client')
+require('./build/server')
+const armor = require('./build/armor')
 
 gulp.task('reload', function (done) {
-    livereload.reload();
-    done();
-});
+    livereload.reload()
+    done()
+})
 
 gulp.task('watch', function (done) {
-    livereload.listen({ port: 3001 });
+    livereload.listen({ port: 3001 })
 
-    gulp.watch('client/styles/**/*.scss', armor(gulp.series('client:styles', 'reload')));
-    gulp.watch('client/assets/**/*', armor(gulp.series('client:assets', 'reload')));
-    gulp.watch('client/svgs/**/*.svg', armor(gulp.series('client:svgs', 'reload')));
-    gulp.watch('client/icons.json', armor(gulp.series('client:icons', 'reload')));
+    gulp.watch('client/styles/**/*.scss', armor(gulp.series('client:styles', 'reload')))
+    gulp.watch('client/assets/**/*', armor(gulp.series('client:assets', 'reload')))
+    gulp.watch('client/svgs/**/*.svg', armor(gulp.series('client:svgs', 'reload')))
+    gulp.watch('client/icons.json', armor(gulp.series('client:icons', 'reload')))
 
-    gulp.watch('code/client/**/*.js', armor(gulp.series('client:code', 'reload')));
-    gulp.watch('code/server/**/*.js', gulp.series('server:restart'));
-    gulp.watch('code/common/**/*.js', gulp.series('server:restart', 'client:code', 'reload'));
+    gulp.watch('code/client/**/*.js', armor(gulp.series('client:code', 'reload')))
+    gulp.watch('code/server/**/*.js', armor(gulp.series('server:restart')))
+    gulp.watch('code/common/**/*.js', armor(gulp.series('server:restart', 'client:code', 'reload')))
 
     process.on('SIGINT', function () {
-        done();
-        process.exit();
-    });
-});
+        done()
+        process.exit()
+    })
+})
 
-gulp.task('build', gulp.parallel('server:build', 'client'));
-gulp.task('default', gulp.series(armor(gulp.parallel('server:restart', 'client')), 'watch'));
+gulp.task('build', gulp.parallel('server:build', 'client'))
+gulp.task('default', gulp.series(armor(gulp.parallel('server:restart', 'client')), 'watch'))
