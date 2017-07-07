9ef2b003d4507f17d5ad784de42035bcf364433a
Ianis G. Vasilev <ianis@ivasilev.net>
2017-07-07 19:36:08 +0300

    Make the sidebar toggle icon sink more on click

--- code/common/components/sidebar_toggle.js
+++ code/common/components/sidebar_toggle.js
@@ -1,13 +1,12 @@
 const { c } = require('common/component')
+const classlist = require('common/support/classlist')
 
 const icon = require('common/components/icon')
 
 module.exports = function sidebarToggle({ isCollapsed, toggleCollapsed }) {
-    return c('div', { class: 'sidebar-toggle button' },
-        c(icon, {
-            name: 'chevron-left',
-            class: isCollapsed && 'collapsed',
-            click: toggleCollapsed
-        })
-    )
+    return c(icon, {
+        name: 'chevron-left',
+        class: classlist('sidebar-toggle', 'button', isCollapsed && 'collapsed'),
+        click: toggleCollapsed
+    })
 }
