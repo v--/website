31efa85e086c3ef50374f4792b6e85b32dafcb42
Ianis G. Vasilev <ianis@ivasilev.net>
2018-06-25 00:45:05 +0300

    Restyle the page borders

--- code/common/components/icon.mjs
+++ code/common/components/icon.mjs
@@ -10,7 +10,11 @@ class SVGComponent extends XMLComponent {
 export default function icon (state) {
   const rootState = Object.assign({ viewBox: '0 0 24 24' }, state)
 
-  if ('class' in state) { rootState.class = `icon ${state.class}` } else { rootState.class = 'icon' }
+  if ('class' in state) {
+    rootState.class = `icon ${state.class}`
+  } else {
+    rootState.class = 'icon'
+  }
 
   return SVGComponent.safeCreate('svg', rootState,
     SVGComponent.safeCreate('path', { d: icons[state.name] })
