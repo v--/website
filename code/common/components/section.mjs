84880e1194496c13f9ac5913ba40017f66c077cb
Ianis G. Vasilev <ianis@ivasilev.net>
2018-06-25 01:14:09 +0300

    Add some ellipses

--- code/common/components/section.mjs
+++ code/common/components/section.mjs
@@ -2,7 +2,7 @@ import { c } from '../component'
 
 export default function section ({ title }, children) {
   return c('div', { class: 'section' },
-    c('h1', { text: title }),
+    c('h1', { class: 'section-title', text: title }),
     ...children
   )
 }
