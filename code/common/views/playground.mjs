0065c8a434636249344f125058c9244c9c57a99e
Ianis G. Vasilev <ianis@ivasilev.net>
2018-06-28 16:58:28 +0300

    Add a description for the playground

--- code/common/views/playground.mjs
+++ code/common/views/playground.mjs
@@ -1,10 +1,16 @@
 import { c } from '../component'
 import section from '../components/section'
 import link from '../components/link'
+import markdown from '../components/markdown'
 
 export default function playground () {
   return c('div', { class: 'page playground-page' },
     c(section, { title: '/playground' },
+      c(markdown, {
+        text: [
+          'These are some JavaScript visualizations and simulations I have programmed across the years.'
+        ].join(' ')
+      }),
       c('ul', null,
         c('li', null,
           c(link, {
