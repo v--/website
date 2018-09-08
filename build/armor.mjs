220993d814a20302ac521e2134412a07d23f37b6
Ianis Vasilev <ianis@ivasilev.net>
2018-09-08 21:21:31 +0300

    Remove libnotify

--- build/armor.mjs
+++ build/armor.mjs
@@ -1,14 +1,6 @@
-import libnotify from 'libnotify'
-
 export default function armor (task) {
   function armored (done) {
-    task(function (error) {
-      if (error) {
-        libnotify.notify(error.message, {
-          title: 'Gulp error'
-        })
-      }
-
+    task(function () {
       done()
     })
   }
