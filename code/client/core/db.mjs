0a6e86cdfa3cdfd286d9411d542fb7343c8d715c
Ianis Vasilev <ianis@ivasilev.net>
2018-09-10 23:49:27 +0300

    Remove API caching

--- code/client/core/db.mjs
+++ code/client/core/db.mjs
@@ -1,42 +1,29 @@
 import { NotFoundError } from '../../common/errors.mjs'
 
-import Cache from './support/cache.mjs'
+async function fetchJSON (url) {
+  const response = await window.fetch(url)
+
+  if (response.status === 404) {
+    throw new NotFoundError()
+  }
+
+  return response.json()
+}
 
 export default class DB {
   constructor ({ data, dataURL }) {
-    this.cache = new Cache(60 * 1000)
     this.collections = {
       files: {
-        readDirectory: async (path) => {
-          return this.fetchJSON(`/api/files/${path}`)
+        readDirectory: (path) => {
+          return fetchJSON(`/api/files/${path}`)
         }
       },
 
       pacmanPackages: {
-        load: async () => {
-          return this.fetchJSON('/api/pacman')
+        load: () => {
+          return fetchJSON('/api/pacman')
         }
       }
     }
-
-    if (dataURL) {
-      this.cache.set(dataURL, data)
-    }
-  }
-
-  async fetchJSON (url) {
-    if (this.cache.has(url)) {
-      return this.cache.get(url)
-    }
-
-    const response = await window.fetch(url)
-
-    if (response.status === 404) {
-      throw new NotFoundError()
-    }
-
-    const json = await response.json()
-    this.cache.set(url, json)
-    return json
   }
 }
