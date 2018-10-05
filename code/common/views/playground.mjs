dc947db5a33b5e6bee62e4c3426e8ff8b74a0495
Ianis Vasilev <ianis@ivasilev.net>
2018-10-05 19:45:16 +0300

    Add a note that the playground code is viewable in a browser's console

--- code/common/views/playground.mjs
+++ code/common/views/playground.mjs
@@ -5,7 +5,7 @@ export default function playground () {
   return c('div', { class: 'page playground-page' },
     c('div', { class: 'section' },
       c('h1', { class: 'section-title', text: '/playground' }),
-      c('p', { text: 'These are some of the JavaScript visualizations and simulations I have created.' }),
+      c('p', { text: "These are some of the JavaScript visualizations and simulations I have created. You can view their code in your browser's console." }),
       c('ul', null,
         c('li', null,
           c(link, {
