7d5b8864656c398e10819e665dfc4d626f17dc8f
Ianis Vasilev <ianis@ivasilev.net>
2019-04-11 17:59:12 +0300

    Make the markdown component produce an article tag

--- code/common/components/markdown.mjs
+++ code/common/components/markdown.mjs
@@ -4,7 +4,7 @@ import { parseMarkdown } from '../support/markdown/parser.mjs'
 import { buildComponentTree } from '../support/markdown/component_builder.mjs'
 
 export default function markdown ({ source, class: className }) {
-  return c('div', { class: classlist(className, 'markdown') },
+  return c('article', { class: classlist(className, 'markdown') },
     buildComponentTree(parseMarkdown(source))
   )
 }
