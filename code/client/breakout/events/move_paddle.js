3eebd095413637b3cfd6ae3d1f5313c4ba71e57d
Ianis Vasilev <ianis@ivasilev.net>
2019-08-15 02:44:47 +0300

    Stop the paddle if it is too close to the ball

--- code/client/breakout/events/move_paddle.js
+++ code/client/breakout/events/move_paddle.js
@@ -4,13 +4,18 @@ import Ellipse from '../geom/ellipse.js'
 import { MOVEMENT_DELTA } from '../constants.js'
 
 export default function movePaddle (subject) {
-  const { status, stage, paddle, paddleDirection } = subject.value
+  const { status, ball, stage, paddle, paddleDirection } = subject.value
 
   if (status !== GameStatus.RUNNING || paddleDirection === 0) {
     return
   }
 
   const candidateX = paddle.center.x + paddleDirection * MOVEMENT_DELTA
+
+  if (Math.abs(ball.center.y - paddle.center.y) < Math.max(2 * ball.radius, paddle.axes.y) && Math.abs(candidateX - ball.center.x) < paddle.axes.x + ball.radius) {
+    return
+  }
+
   const edge = paddleDirection * (stage.size.x / 2 - paddle.axes.x)
 
   const newX = (paddleDirection < 0 && candidateX >= edge) || (paddleDirection > 0 && candidateX <= edge) ? candidateX : edge
