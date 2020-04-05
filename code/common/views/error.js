7f4cbc37392d88a0331a2859d29889419ef38d12
Ianis Vasilev <ianis@ivasilev.net>
2020-04-05 17:12:57 +0300

    FIx error layout

--- code/common/views/error.js
+++ code/common/views/error.js
@@ -3,7 +3,6 @@ import { c } from '../rendering/component.js'
 
 import { icon } from '../components/icon.js'
 import { link } from '../components/link.js'
-import { sectionTitle } from '../components/section_title.js'
 
 export function error ({ data: err }) {
   const title = CoolError.isDisplayable(err) ? err.message : 'Error'
@@ -11,7 +10,7 @@ export function error ({ data: err }) {
   return c('div', { class: 'page error-page' },
     c('br'),
     c(icon, { class: 'alert', name: 'alert' }),
-    c(sectionTitle, { text: title }),
+    c('h1', { class: 'h1', text: title }),
     c('p', null,
       c('span', { text: 'Please try refreshing the browser or ' }),
       c(link, { text: 'reporting a bug', link: 'mailto:ianis@ivasilev.net.' })
