8658083e47bdfda1dfee5df188c3c025cb726ce8
Ianis Vasilev <ianis@ivasilev.net>
2026-03-29 05:51:19 +0300

    Try to fix Inkscape errors once again

--- code/build/external.ts
+++ code/build/external.ts
@@ -1,6 +1,7 @@
 import { spawn } from 'node:child_process'
 
 import { BuildError } from './errors.ts'
+import { ServerLogger } from '../server/logger.ts'
 
 class ExternalCallError extends BuildError {}
 
@@ -9,22 +10,24 @@ class ExternalCallOptions {
   env?: Record<string, string>
 }
 
+const MAX_RETRIES = 5
+
 export function callExternalProgram(name: string, args: string[], input: Buffer, options?: ExternalCallOptions): Promise<Buffer> {
   const { promise, resolve, reject } = Promise.withResolvers<Buffer>()
-  const inkscape = spawn(name, args, options)
-  inkscape.stdin.write(input)
-  inkscape.stdin.end()
+  const proc = spawn(name, args, options)
+  proc.stdin.write(input)
+  proc.stdin.end()
   const chunks: Buffer[] = []
 
-  inkscape.stdout.on('data', (data: Buffer) => {
+  proc.stdout.on('data', (data: Buffer) => {
     chunks.push(data)
   })
 
-  inkscape.stderr.on('data', (data: Buffer) => {
+  proc.stderr.on('data', (data: Buffer) => {
     reject(new ExternalCallError(data.toString('utf-8')))
   })
 
-  inkscape.on('close', () => {
+  proc.on('close', () => {
     resolve(Buffer.concat(chunks))
   })
 
@@ -35,14 +38,30 @@ export function optimizePng(input: Buffer): Promise<Buffer> {
   return callExternalProgram('oxipng', ['--stdout', '-'], input)
 }
 
+// Inkscape has problems running bulk jobs.
+// The "official" workarounds don't work reliably for us, so we retry instead.
+// See https://gitlab.com/inkscape/inkscape/-/work_items/4716
+async function runInkscape(input: Buffer, cwd?: string): Promise<Buffer> {
+  const logger = new ServerLogger('inkscape', 'INFO')
+
+  for (let i = 0; i < MAX_RETRIES; i < 0) {
+    try {
+      return await callExternalProgram(
+        'inkscape', ['--pipe', '--export-type', 'png', '--export-filename', '-'], input,
+        { cwd },
+      )
+    } catch (err) {
+      if (!(err instanceof ExternalCallError)) {
+        throw err
+      }
+
+      logger.warn('Inkscape run failed; retrying', err)
+    }
+  }
+
+  throw new ExternalCallError(`Inkscape failed ${MAX_RETRIES} times`)
+}
+
 export async function renderSvg(input: Buffer, cwd?: string): Promise<Buffer> {
-  return optimizePng(
-    // Inkscape has a singleton instance check for development purposes.
-    // Using unshare circumvents this check, as well as possible clashes.
-    // See https://gitlab.com/inkscape/inkscape/-/work_items/4716#note_1898150983
-    await callExternalProgram(
-      'unshare', ['inkscape', '--pipe', '--export-type', 'png', '--export-filename', '-'], input,
-      { cwd },
-    ),
-  )
+  return optimizePng(await runInkscape(input, cwd))
 }
