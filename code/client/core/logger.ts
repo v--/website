eb11fab98671480908fb0fe314991ae8709c1ddc
Ianis Vasilev <ianis@ivasilev.net>
2026-06-19 17:25:22 +0300

    Revise logging output

--- code/client/core/logger.ts
+++ code/client/core/logger.ts
@@ -1,11 +1,16 @@
 /* eslint-disable no-console */
 
-import { Logger, type LoggerLevel } from '../../common/logger.ts'
+import { Logger, type LoggerLevel, formatDate, formatLoggerLevel } from '../../common/logger.ts'
 import { intersperse } from '../../common/support/iteration.ts'
 
 export class ClientLogger extends Logger {
   write(level: LoggerLevel, ...contents: unknown[]) {
     const key = level.toLowerCase() as Lowercase<LoggerLevel>
-    console[key](this.generatePrefix(), ...intersperse(contents, '\n'))
+    console[key](
+      formatDate(new Date(), { includeDate: false }),
+      formatLoggerLevel(level),
+      `[${this.subject}]`,
+      ...intersperse(contents, '\n'),
+    )
   }
 }
--- code/common/logger.ts
+++ code/common/logger.ts
@@ -1,39 +1,59 @@
 import { type uint32 } from './types/numbers.ts'
 
-const LOGGER_LEVELS = ['DEBUG', 'INFO', 'WARN', 'ERROR'] as const
+export const LOGGER_LEVELS = ['DEBUG', 'INFO', 'WARN', 'ERROR'] as const
 export type LoggerLevel = typeof LOGGER_LEVELS[uint32]
+
+const MAX_LOG_LEVEL_LENGTH = Math.max(...LOGGER_LEVELS.map(level => level.length))
+
+export function formatLoggerLevel(level: LoggerLevel) {
+  return level.padEnd(MAX_LOG_LEVEL_LENGTH)
+}
 
 function padDateValue(value: uint32) {
   return String(value).padStart(2, '0')
 }
 
+interface IFormatDateOptions {
+  includeDate?: boolean
+}
+
+export function formatDate(date: Date, { includeDate }: IFormatDateOptions = {}) {
+  // TODO@2030: Update this when Temporal is widely available
+  // Date#toISOString() is close to what we need but it doesn't respect time zones
+
+  let result = ''
+
+  if (includeDate) {
+    const Y = date.getFullYear()
+    const m = padDateValue(date.getMonth() + 1)
+    const d = padDateValue(date.getDate())
+    result += `${Y}-${m}-${d}`
+  }
+
+  if (result.length > 0) {
+    result += ' '
+  }
+
+  const h = padDateValue(date.getHours())
+  const M = padDateValue(date.getMinutes())
+  const s = padDateValue(date.getSeconds())
+  result += `${h}:${M}:${s}`
+
+  return result
+}
+
 export abstract class Logger {
-  readonly name: string
+  readonly subject: string
   readonly level: LoggerLevel
   #levelIndex: uint32
 
-  constructor(name: string, level: LoggerLevel) {
-    this.name = name
+  constructor(subject: string, level: LoggerLevel) {
+    this.subject = subject
     this.level = level
     this.#levelIndex = LOGGER_LEVELS.indexOf(level)
   }
 
   abstract write(level: LoggerLevel, ...contents: unknown[]): void
-
-  generatePrefix() {
-    // TODO@2030: Update this when Temporal is widely available
-    // Date#toISOString() is close to what we need but it doesn't respect time zones
-    const date = new Date()
-    const Y = date.getFullYear()
-    const m = padDateValue(date.getMonth() + 1)
-    const d = padDateValue(date.getDate())
-    const h = padDateValue(date.getHours())
-    const M = padDateValue(date.getMinutes())
-    const s = padDateValue(date.getSeconds())
-    const dateString = `${Y}-${m}-${d} ${h}:${M}:${s}`
-
-    return `${dateString} [${this.name}]`
-  }
 
   log(level: LoggerLevel, ...contents: unknown[]) {
     if (LOGGER_LEVELS.indexOf(level) >= this.#levelIndex) {
